6670bf12c568f6aa42346e2bdc5cf7b41414d692
Mark <mark.matern@vtex.com.br>
2018-08-29 17:14:36 -0300

    Remove unused code

--- react/components/Messages.js
+++ react/components/Messages.js
@@ -7,20 +7,10 @@ class Messages extends Component {
     const { message, type, clearMessage } = this.props
 
     return (
-      // <div className="w-100 pt3">
-      //   <div className={`w-80-ns center br2 pv4 ph5 mb4 ${typeClass}`}>
-      //     {message}
-      //     <a className="item-link-remove" onClick={clearMessage}>
-      //       <i className="icon icon-remove item-remove-ico fr"></i>
-      //     </a>
-      //   </div>
-      // </div>
       <div>
-        <div className="">
-          <Alert type={type} onClose={clearMessage}>
-            {message}
-          </Alert>
-        </div>
+        <Alert type={type} onClose={clearMessage}>
+          {message}
+        </Alert>
       </div>
     )
   }
