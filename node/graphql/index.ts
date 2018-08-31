3cb1260f836aac90b44b7b13dc19f8d15d72d98b
Mark <mark.matern@vtex.com.br>
2018-08-31 15:21:59 -0300

    Add MasterData schema check. Create schema when necessary

--- node/graphql/index.ts
+++ node/graphql/index.ts
@@ -4,17 +4,29 @@ import * as jwt from 'jsonwebtoken'
 import colossus from '../resources/colossus'
 import {errorResponse} from '../utils/error'
 
-const appMajor = '0'
-
 const getAppId = () => {
-  return `vtex.savecart@${appMajor}.x`
+  return process.env.VTEX_APP_ID
 }
 
 const routes = {
   saveCart: (account) => `http://${account}.vtexcommercestable.com.br/api/dataentities/cart/documents`,
-  listCarts: (account, email) => `http://${account}.vtexcommercestable.com.br/api/dataentities/cart/search?email=${email}&_schema=v4&_fields=id,email,cartName,items,creationDate`,
+  listCarts: (account, email) => `http://${account}.vtexcommercestable.com.br/api/dataentities/cart/search?email=${email}&_schema=v5&_fields=id,email,cartName,items,creationDate`,
   removeCart: (account, id) => `http://${account}.vtexcommercestable.com.br/api/dataentities/cart/documents/${id}`,
+  saveSchema: (account) => `http://${account}.vtexcommercestable.com.br/api/dataentities/cart/schemas/v5`,
 }
+
+const schema = `{
+  "properties": {
+  "email": { "type": "string", "title":"Email" },
+  "cartName": { "type": "string", "title":"Cart Name" },
+  "items": { "type": "array", "title":"Cart" },
+  "creationDate": { "type": "string", "title":"Creation Date" },
+  "cartLifeSpan": { "type": "string", "title":"Cart Life Span" }
+  },
+  "v-indexed": [ "email", "creationDate", "cartLifeSpan", "cartName" ],
+  "v-default-fields": [ "email", "cart", "creationDate", "cartLifeSpan" ],
+  "v-cache": false
+  }`
 
 const defaultHeaders = (authToken) => ({
   'Content-Type': 'application/json',
@@ -26,9 +38,32 @@ const defaultHeaders = (authToken) => ({
 export const resolvers = {
   Query: {
     getSetupConfig: async (_, __, ctx) => {
+      const {vtex: {account, authToken}} = ctx
       const apps = new Apps(ctx.vtex)
-      const filter = getAppId()
-      return apps.getAppSettings(filter).then((r) => (r))
+      const app = getAppId()
+      const settings = await apps.getAppSettings(app)
+      if (settings.adminSetup && !settings.adminSetup.hasSchema) {
+        try {
+          console.log('Starting to put schema in MD')
+          const url = routes.saveSchema(account)
+          const headers = defaultHeaders(authToken)
+          await http({
+            method: 'PUT',
+            url,
+            data: schema,
+            headers,
+            validateStatus: status => (status >= 200 && status < 300) || status === 304,
+          })
+          console.log('Schema saved successfully. Updating app settings')
+          settings.adminSetup.hasSchema = true
+          await apps.saveAppSettings(app, settings)
+        } catch (e) {
+          console.log('PutSchemaResponseError: ', e)
+          settings.adminSetup.hasSchema = false
+          await apps.saveAppSettings(app, settings)
+        }
+      }
+      return settings
     },
     currentTime: async (_, __, ___) => {
       return new Date().toISOString()
@@ -77,9 +112,10 @@ export const resolvers = {
         logger.log('CartListSuccess', 'info', {user: params.email})
         return data
       } catch (e) {
+        console.log(e)
         const {status, body, details} = errorResponse(e)
         logger.log('CartListError', 'error', {user: params.email, status, body, details})
-        throw {status, body, details}
+        throw e
       }
     },
 
