5d3d0941f0581ad2a962ff36994d46d0d0217d4b
artur.pimentel <artur.oliveira@vtex.com.br>
2019-07-16 20:09:06 -0300

    Create REST APIs from resolvers

---
+++ node/index.ts
@@ -0,0 +1,67 @@
+import * as json from 'co-body'
+import { mapObjIndexed } from 'ramda'
+
+import { resolvers } from './graphql'
+
+export default {
+  routes: {
+    ...mapObjIndexed(
+      handler => async (ctx: any) => {
+        try {
+          ctx.body = await handler({}, ctx.vtex.route.params, ctx)
+          ctx.status = 200
+        } catch (e) {
+          ctx.body = e
+          ctx.status = 500
+        }
+      },
+      {
+        ...resolvers.Query,
+        getCarts: resolvers.Mutation.getCarts
+      }
+    ),
+    saveCart: async (ctx: any) => {
+      try {
+        ctx.body = await resolvers.Mutation.saveCart({}, {
+          cart: await json(ctx.req)
+        }, ctx)
+        ctx.status = 200
+      } catch (e) {
+        ctx.body = e
+        ctx.status = 500
+      }
+    },
+    removeCart: async (ctx: any) => {
+      try {
+        const { cartName, expired } = await json(ctx.req)
+        const { id } = ctx.vtex.route.params
+
+        ctx.body = await resolvers.Mutation.removeCart({}, {
+          cartName,
+          expired,
+          id
+        }, ctx)
+        ctx.status = 200
+      } catch (e) {
+        ctx.body = e
+        ctx.status = 500
+      }
+    },
+    useCart: async (ctx: any) => {
+      try {
+        const { items, userType } = await json(ctx.req)
+        const { orderFormId } = ctx.vtex.route.params
+
+        ctx.body = await resolvers.Mutation.useCart({}, {
+          items,
+          userType,
+          orderFormId,
+        }, ctx)
+        ctx.status = 200
+      } catch (e) {
+        ctx.body = e
+        ctx.status = 500
+      }
+    },
+  }
+}
